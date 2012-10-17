d0479399bb9d754d90cecb31b311873b1152623a
Shailesh Patil <shailesh@joshsoftware.com>
2012-10-17 15:18:19 +0530

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Viki::Application.config.secret_token = '1569acf0d9366cfe1f2970924427e2d620665907062a22f3cdbfacc80f9a32fa84d45261076ba706229e76e77326498210db7901e3064aa00b2cc264446c9af4'
